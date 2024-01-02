88cd44ba6939d5efb807d2112f801253f1c7fd2b
momen124 <momenhossny2004@gmail.com>
2024-01-02 22:29:49 +0200

    fix

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python3
 import random
+
 number = random.randint(-10, 10)
-if number > 0:
-    print(f"{number :d} is positive")
-elif number == 0:
-    print(f"{number :d} is zero")
-else:
-    print(f"{number :d} is negative")
+
+try:
+    if number > 0:
+        print(f"{number} is positive")
+    elif number == 0:
+        print(f"{number} is zero")
+    else:
+        print(f"{number} is negative")
+except TypeError:
+    print("TypeError")
--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
 import random
+
 i = random.randint(-10000, 10000)
 last = abs(i) % 10
+
 if i < 0:
     last = -last
+
 if last > 5:
-    print(f"Last digit of {i :d} is {last :d} and is greater than 5")
+    print(f"Last digit of {i} is {last} and is greater than 5")
 elif last == 0:
-    print(f"Last digit of {i :d} is {last :d} and is 0")
+    print(f"Last digit of {i} is {last} and is 0")
 else:
-    print(f"Last digit of {i :d} is {last :d} and is less than 6 and not 0")
+    print(f"Last digit of {i} is {last} and is less than 6 and not 0")
