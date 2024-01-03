32fc9060ca913f8717c10ab5e28b9fbc5ea7992f
momen124 <momenhossny2004@gmail.com>
2024-01-03 02:22:24 +0200

    fix

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 for i in range(122, 96, -1):
-    print("{}".format(chr(i).upper() if (122 - i) % 4 < 2 else chr(i)), end="")
+    print("{}".format(chr(i - 32) if (122 - i) % 2 == 0 else chr(i)), end="")
