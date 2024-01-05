ef95c3731affe688b148a18ff04e1514a4fecc65
momen124 <momenhossny2004@gmail.com>
2024-01-05 15:29:16 +0200

    fix

--- 0x02-python-import_modules/hidden_4.py
+++ 0x02-python-import_modules/hidden_4.py
@@ -4,9 +4,8 @@ if __name__ == "__main__":
     try:
         import hidden_4
 
-        names = dir(hidden_4)
-        for name in names:
-            if name[:2] != "__":
-                print(name)
+        names = [name for name in dir(hidden_4) if not name.startswith("__")]
+        for name in sorted(names):
+            print(name)
     except ImportError as e:
         print(f"Error importing module: {e}")
