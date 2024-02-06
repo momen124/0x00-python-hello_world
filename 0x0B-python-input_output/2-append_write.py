166ee3ca4aa926a0eae32fc8884f1e6bba053640
momen124 <momenhossny2004@gmail.com>
2024-02-06 16:48:03 +0200

    input&output

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+def append_write(filename="", text=""):
+    with open(filename, "a", encoding="utf-8") as filee:
+        return filee.write(text)
---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+import json
+def from_json_string(my_str):
+    string_returned = json.loads(my_str)
+    return string_returned
