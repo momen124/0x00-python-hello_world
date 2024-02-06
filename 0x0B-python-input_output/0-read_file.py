70b085789db89119d75e1602c848d604dffab594
momen124 <momenhossny2004@gmail.com>
2024-02-06 17:20:58 +0200

    fixing

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,18 +1,8 @@
 #!/usr/bin/python3
-"""module_0"""
+"""Defines a text file-reading function."""
 
 
 def read_file(filename=""):
-    """Reads the content of a text file and prints it to stdout.
-
-    Args:
-        filename (str): The path to the text file to be read. Defaults to an empty string.
-
-    Returns:
-        None
-
-    Prints:
-        The content of the text file to the standard output (stdout).
-    """
-    with open(filename, 'r', encoding='utf-8') as file:
-        print(file.read(), end='')
+    """Print the contents of a UTF8 text file to stdout."""
+    with open(filename, encoding="utf-8") as f:
+        print(f.read(), end="")
