8ffaa30737fb4d9cd3ee35025593ae67f0f84163
momen124 <momenhossny2004@gmail.com>
2024-02-12 21:54:12 +0200

    fix

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -4,52 +4,80 @@ from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """Represent a square, which is a special rectangle."""
+    """Represent a square."""
 
     def __init__(self, size, x=0, y=0, id=None):
         """Initialize a new Square.
 
         Args:
-            size (int): The size of the square.
-            x (int, optional): The x-coordinate of the square's position.
-            y (int, optional): The y-coordinate of the square's position.
-            id (int, optional): The identity of the square.
+            size (int): The size of the new Square.
+            x (int): The x coordinate of the new Square.
+            y (int): The y coordinate of the new Square.
+            id (int): The identity of the new Square.
         """
         super().__init__(size, size, x, y, id)
 
-    def __str__(self):
-        """Return a string representation of the Square."""
-        return "[Square] ({}) {}/{} - {}".format(
-            self.id, self.x, self.y, self.width)
+    @property
+    def size(self):
+        """Get/set the size of the Square."""
+        return self.width
 
-    def area(self):
-        """Return the area of the square."""
-        return self.width ** 2
-
-    def perimeter(self):
-        """Return the perimeter of the square."""
-        return 4 * self.width
+    @size.setter
+    def size(self, value):
+        self.width = value
+        self.height = value
 
     def update(self, *args, **kwargs):
-        """Update the attributes of the Square."""
-        if args:
-            attrs = ["id", "size", "x", "y"]
-            for attr, value in zip(attrs, args):
-                setattr(self, attr, value)
-        elif kwargs:
-            for key, value in kwargs.items():
-                setattr(self, key, value)
+        """Update the Square.
+
+        Args:
+            *args (ints): New attribute values.
+                - 1st argument represents id attribute
+                - 2nd argument represents size attribute
+                - 3rd argument represents x attribute
+                - 4th argument represents y attribute
+            **kwargs (dict): New key/value pairs of attributes.
+        """
+        if args and len(args) != 0:
+            a = 0
+            for arg in args:
+                if a == 0:
+                    if arg is None:
+                        self.__init__(self.size, self.x, self.y)
+                    else:
+                        self.id = arg
+                elif a == 1:
+                    self.size = arg
+                elif a == 2:
+                    self.x = arg
+                elif a == 3:
+                    self.y = arg
+                a += 1
+
+        elif kwargs and len(kwargs) != 0:
+            for k, v in kwargs.items():
+                if k == "id":
+                    if v is None:
+                        self.__init__(self.size, self.x, self.y)
+                    else:
+                        self.id = v
+                elif k == "size":
+                    self.size = v
+                elif k == "x":
+                    self.x = v
+                elif k == "y":
+                    self.y = v
 
     def to_dictionary(self):
         """Return the dictionary representation of the Square."""
         return {
-            'id': self.id,
-            'size': self.size,
-            'x': self.x,
-            'y': self.y
+            "id": self.id,
+            "size": self.width,
+            "x": self.x,
+            "y": self.y
         }
 
-
-def perimeter(self):
-    """Return the perimeter of the square."""
-    return 4 * self.width
+    def __str__(self):
+        """Return the print() and str() representation of a Square."""
+        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y,
+                                                 self.width)
