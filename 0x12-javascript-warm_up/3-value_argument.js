0969b9480ae984b7f4cdd5af0ed37160cbbbf3e5
momen124 <momenhossny2004@gmail.com>
2024-04-09 00:17:58 +0200

    js

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
-function printArgs (argv) {
-    if (process.argv[2] === undefined) {
-      console.log('No argument');
-    } else {
-      console.log(process.argv[2]);
-    }
+function printArgs(argv) {
+  if (process.argv[2] === undefined) {
+    console.log('No argument');
+  } else {
+    console.log(process.argv[2]);
   }
-  printArgs();
+}
+printArgs();
